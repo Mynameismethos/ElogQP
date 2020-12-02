d177fcdc205649352b8ff962b7d5c391ea24713a
Lennart <Lennart@Laptop>
2020-12-02 22:28:32 +0100

    BA aufbau

--- Eventlog.py
+++ Eventlog.py
@@ -2,4 +2,4 @@ import pm4py
 
 log = pm4py.read_xes("LOGEDV.xes")
 
-print(log [0][0])
+print(log[0][0])
--- helloword.py
+++ helloword.py
@@ -1,42 +1,43 @@
-# pip install pm4py 
-
-#black formatter
-#pylant 
+# pylant
 import tkinter
-from tkinter.constants import INSERT 
+from tkinter.constants import INSERT
 import pm4py
-import importlib 
+import importlib
 from tkinter import Button, Entry, Label, Tk
 
 frame = Tk()
 
 frame.geometry("300x300+100+100")
-myText = Label(frame, text='Hello World')
+myText = Label(frame, text="Hello World")
 entry = Entry(frame)
 entry.pack()
 entry.insert(INSERT, "Path to XES")
 
 
-def display_hello(): 
+def display_hello():
     myText.pack()
+
+
 def remove_hello():
     myText.pack_forget()
 
+
 def import_xes_Log():
-    log=pm4py.read_xes(entry.get())
+    log = pm4py.read_xes(entry.get())
+
 
 def importModule():
-    mod= importlib.import_module(entry.get())
-    test= mod.myhello()
+    mod = importlib.import_module(entry.get())
+    test = mod.myhello()
     print(test)
-    
 
-hello_button = Button(frame, text="hello Button", command = display_hello)
+
+hello_button = Button(frame, text="hello Button", command=display_hello)
 hello_button.pack()
-forget_button = Button(frame, text="Remove Button", command = remove_hello)
+forget_button = Button(frame, text="Remove Button", command=remove_hello)
 forget_button.pack()
-load_Log_button = Button(frame, text="Load XES", command = import_xes_Log)
-load_Log_button = Button(frame, text="Load Module", command = importModule)
+load_Log_button = Button(frame, text="Load XES", command=import_xes_Log)
+load_Log_button = Button(frame, text="Load Module", command=importModule)
 load_Log_button.pack()
 print("Hello world")
 frame.mainloop()
