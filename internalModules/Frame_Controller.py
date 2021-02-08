4f33ac6ce7977b6d39103219d63b7b124709d86c
Lennart PC <lennart>
2021-02-08 18:15:29 +0100

    Seperated Start From TK

---
+++ internalModules/Frame_Controller.py
@@ -0,0 +1,96 @@
+import internalModules.objects as objects
+import tkinter as tk
+import Frames.frame_start
+import Frames.frame_modules
+import Frames.frame_showError
+
+class Frame_Controller(tk.Tk):
+    def __init__(self,*args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.winfo_toplevel().title("Lennart Brandt")
+        self.container = tk.Frame(self)
+        self.container.pack(side="top", fill="both", expand=True)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        self.activeFrames = []
+        for F in (Frames.frame_start.frame_start, Frames.frame_modules.frame_modules, Frames.frame_showError.frame_showError):
+            self.createFrame(F, F.__name__)
+        
+        self.showFrame("frame_start")  
+  
+
+    def showFrame(self, cont):
+        display = self.data.frames[cont]
+        self.activeFrames.append(display)
+        display.tkraise()
+        display.showMe()
+
+
+    def showPrevFrame(self):
+        if(self.hasPrevFrame()):
+            self.activeFrames.pop()
+            self.activeFrames[-1].tkraise()
+        else:
+            self.showFrame("frame_start")
+
+    def hasPrevFrame(self):
+        self.deleteDouble()
+        return (len(self.activeFrames) > 1)
+
+    def deleteDouble(self):
+        prev=None
+        for x in reversed(self.activeFrames):
+            if(prev):
+                if(isinstance(x ,prev.__class__)):
+                    del self.activeFrames[self.activeFrames.index(prev)]
+            prev=x
+
+    def createFrame(self, module, name):
+        if name not in self.data.frames:
+            display = module(self.container, self)
+            display.grid(row=0, column=0, sticky="nsew")
+            self.data.frames[name] = display
+
+    def getFrameByName(self, frameName):
+        return self.data.frames[frameName]
+
+    def delFrameByName(self, frameName):
+        self.data.frames[frameName].destroy()
+        del self.data.frames[frameName]
+
+    ###Controlling Modular Frames######
+
+    def showModFrame(self, name,next=False, prev=False):
+        display=None
+        if(next):
+            if(self.data.activeMod[name].modFramesNext):
+                self.data.activeMod[name].modFramesPrev.append(self.data.activeMod[name].modFramesNext.pop())
+            else:
+                self.getFrameByName("frame_modules").showNextMod()
+        elif(prev):
+            if(len(self.data.activeMod[name].modFramesPrev)>1):
+                self.data.activeMod[name].modFramesNext.append(self.data.activeMod[name].modFramesPrev.pop())
+            else: self.showPrevFrame()
+        else: 
+            pass
+
+        display = self.data.activeMod[name].modFramesPrev[-1]
+        display.tkraise()
+        display.showMe()
+
+    def deleteModFrame(self,name):
+        del self.data.activeMod[name]
+
+
+    def createModFrame(self, number,name):
+        frame = self.data.modFrames[number]
+        display = frame(self.container, self)
+        display.grid(row=0, column=0, sticky="nsew")
+        if(name not in self.data.activeMod):self.data.activeMod[name]= objects.module()
+        self.data.activeMod[name].modFramesNext.append(display)
+
+    def getActiveModFrame(self,name):
+        return self.data.activeMod[name].modFramesPrev[-1]
+
+    def getNextModFrame(self,name):
+        return self.data.activeMod[name].modFramesNext[-1]
--- start.py
+++ start.py
@@ -1,111 +1,25 @@
-import tkinter as tk
-import Frames.frame_start
-import Frames.frame_modules
-import Frames.frame_showError
+from internalModules.Frame_Controller import Frame_Controller
 import internalModules.loadmodules as loadmodules
-
 import internalModules.loadLog as loadLog
-import internalModules.Data as data
-import internalModules.objects as objects
+import internalModules.Data as dataBase
 
 
 #init the Display and load the singular fames
-class Display(tk.Tk):
+class Display(Frame_Controller):
     def __init__(self, *args, **kwargs):
+        self.data=dataBase
+        self.data.modFrames = loadmodules.loadFrames()
+        Frame_Controller.__init__(self, *args, **kwargs)
 
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.winfo_toplevel().title("Lennart Brandt")
-        self.container = tk.Frame(self)
-        self.container.pack(side="top", fill="both", expand=True)
-        self.container.grid_rowconfigure(0, weight=1)
-        self.container.grid_columnconfigure(0, weight=1)
-        self.activeFrames = []
-
-        for F in (Frames.frame_start.frame_start, Frames.frame_modules.frame_modules, Frames.frame_showError.frame_showError):
-            self.createFrame(F, F.__name__)
-        data.modFrames = loadmodules.loadFrames()
-        self.showFrame("frame_start")
-
-    def showFrame(self, cont):
-        display = data.frames[cont]
-        self.activeFrames.append(display)
-        display.tkraise()
-        display.showMe()
-
-    def showModFrame(self, name,next=False, prev=False):
-        display=None
-        if(next):
-            if(data.activeMod[name].modFramesNext):
-                data.activeMod[name].modFramesPrev.append(data.activeMod[name].modFramesNext.pop())
-            else:
-                self.getFrameByName("frame_modules").showNextMod()
-        elif(prev):
-            if(len(data.activeMod[name].modFramesPrev)>1):
-                data.activeMod[name].modFramesNext.append(data.activeMod[name].modFramesPrev.pop())
-            else: self.showPrevFrame()
-        else: 
-            pass
-
-        display = data.activeMod[name].modFramesPrev[-1]
-        display.tkraise()
-        display.showMe()
-
-    def deleteModFrame(self,name):
-        del data.activeMod[name]
-
-    def showPrevFrame(self):
-        if(self.hasPrevFrame()):
-            self.activeFrames.pop()
-            self.activeFrames[-1].tkraise()
-        else:
-            self.showFrame("frame_start")
-
-    def hasPrevFrame(self):
-        self.deleteDouble()
-        return (len(self.activeFrames) > 1)
-
-    def deleteDouble(self):
-        prev=None
-        for x in reversed(self.activeFrames):
-            if(prev):
-                if(isinstance(x ,prev.__class__)):
-                    del self.activeFrames[self.activeFrames.index(prev)]
-            prev=x
-
-    def createFrame(self, module, name):
-        if name not in data.frames:
-            display = module(self.container, self)
-            display.grid(row=0, column=0, sticky="nsew")
-            data.frames[name] = display
+        
 
     def addToErrorList(self, modErrorList):
         #Check if Entrys already Exist
         for x in modErrorList:
             hash_x= hash(x)
-            if(hash_x not in data.error_Hash_Dict):
-                data.error_List.append(x)
-                data.error_Hash_Dict[hash_x]=x
-
-    def createModFrame(self, number,name):
-        frame = data.modFrames[number]
-        display = frame(self.container, self)
-        display.grid(row=0, column=0, sticky="nsew")
-        if(name not in data.activeMod):data.activeMod[name]= objects.module()
-        data.activeMod[name].modFramesNext.append(display)
-        
-
-    def getFrameByName(self, frameName):
-        return data.frames[frameName]
-
-    def delFrameByName(self, frameName):
-        data.frames[frameName].destroy()
-        del data.frames[frameName]
-    
-    def getActiveModFrame(self,name):
-        return data.activeMod[name].modFramesPrev[-1]
-
-    def getNextModFrame(self,name):
-        return data.activeMod[name].modFramesNext[-1]
+            if(hash_x not in self.data.error_Hash_Dict):
+                self.data.error_List.append(x)
+                self.data.error_Hash_Dict[hash_x]=x
 
 
     def get_xes_file_list(self):
@@ -113,12 +27,12 @@ class Display(tk.Tk):
         self.getFrameByName("frame_start").updateData(list)
 
     def import_xes_Log(self, button, name):
-        data.log = loadLog.loadLogByName(self,name,button)
+        self.data.log = loadLog.loadLogByName(self,name,button)
 
 
     def importModule(self, button):
-        data.module_List = loadmodules.loadmodules(self)
-        for module in data.module_List:
+        self.data.module_List = loadmodules.loadmodules(self)
+        for module in self.data.module_List:
             print(
                 "This Module is called: '"
                 + module.getName()
@@ -126,29 +40,29 @@ class Display(tk.Tk):
                 + module.getOneDesc()
             )
         moduleName="frame_start"
-        successfull = len(data.module_List) > 0
+        successfull = len(self.data.module_List) > 0
         if successfull:
-            data.frames[moduleName].button_feedback(button, True)
+            self.data.frames[moduleName].button_feedback(button, True)
         else:
-            data.frames[moduleName].button_feedback(button, False)
+            self.data.frames[moduleName].button_feedback(button, False)
 
     def getModules(self):
-        return data.module_List
+        return self.data.module_List
 
     def getErrorList(self):
-        return data.error_List
+        return self.data.error_List
     def clearErrorList(self):
-        data.error_List=[]
-        data.error_Hash_Dict={}
+        self.data.error_List=[]
+        self.data.error_Hash_Dict={}
 
     def getLog(self):
-        return data.log
+        return self.data.log
 
     def setLog(self, log, button=None,name=None):
         if(button):
-            data.frames["frame_start"].button_feedback(button, True)
-            data.frames["frame_start"].updateData(highlight=name)
-        data.log = log
+            self.data.frames["frame_start"].button_feedback(button, True)
+            self.data.frames["frame_start"].updateData(highlight=name)
+        self.data.log = log
 
 
 # Start of Programm
